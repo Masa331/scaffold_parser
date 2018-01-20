5116f71ebf1dc277143a9076cf3fbf3ec88ab3cf
Premysl Donat <pdonat@seznam.cz>
2018-01-20 16:11:01 +0100

    wip

--- lib/scaffold_parser/builder.rb
+++ lib/scaffold_parser/builder.rb
@@ -1,56 +1,80 @@
 module ScaffoldParser
   class Builder
-    def self.call(model, params = {})
-      self.new(model, params).call
+    def self.call(models, params = {})
+      self.new(models, params).call
     end
 
-    def initialize(model, params)
-      @model = model
+    def initialize(models, params)
+      @models = models
       @params = params
     end
 
     def call
-      # require 'pry'; binding.pry
-      # path = @params.delete(:path) || "./tmp/#{@model.file_name}"
+      @models.each do |model|
+        file_name = model.name.underscore
+        path = @params.delete(:path) || "./tmp/#{file_name}.rb"
 
-      puts @model
-      # File.open(path, 'wb') do |f|
-      #   f.puts "class #{@model.name}"
-      #
-      #   methods = at_methods.map do |method|
-      #     at_method_template(method)
-      #   end
-      #   methods.each_with_index do |m, i|
-      #     f.puts m
-      #     f.puts unless i == (methods.size - 1)
-      #   end
-      #
-      #   f.puts "end"
-      # end
+        File.open(path, 'wb') do |f|
+          element_methods(model).each do |n|
+            f.puts "require '#{n.name.underscore}'"
+          end
+
+          f.puts
+          f.puts "class #{model.name.classify}"
+
+          methods = at_methods(model).map do |method|
+            at_method_template(method)
+          end
+          methods.each_with_index do |m, i|
+            f.puts m
+            f.puts unless i == (methods.size - 1)
+          end
+
+          f.puts if element_methods(model).any?
+
+          methods = element_methods(model).map do |method|
+            element_method_template(method)
+          end
+          methods.each_with_index do |m, i|
+            f.puts m
+            f.puts unless i == (methods.size - 1)
+          end
+
+          f.puts "end"
+        end
+      end
     end
 
     private
 
-    def at_methods
-      @model.methods.select { |m| m[:class].blank? }
+    def at_methods(model)
+      model.nodes.select do |m|
+        m.nodes.empty?
+      end
     end
 
-    def element_methods
-      @model.methods.select { |m| m[:class].present? }
+    def element_methods(model)
+      model.nodes.select do |m|
+        m.nodes.any?
+      end
     end
 
     def at_method_template(method)
       <<-DEF
-  def #{method[:name]}
-    at '#{method[:at]}'
+  def #{method.name.underscore}
+    at '#{method.name}'
   end
       DEF
     end
 
     def element_method_template(method)
+      klass = method.type || method.name.classify
+
       <<-DEF
-  def #{method[:name]}
-    at '#{method[:at]}'
+  def #{method.name.underscore}
+    element_xml = at '#{method.name}'
+
+    #{klass}.new(element_xml) if element_xml
   end
       DEF
     end
--- spec/scaffold_parser_spec.rb
+++ spec/scaffold_parser_spec.rb
@@ -1,33 +1,7 @@
 RSpec.describe ScaffoldParser do
   it 'generates parser skeleton from given xsd' do
-    res = ScaffoldParser.scaffold('./spec/fixtures/xsd/invoice.xsd')
-    # require 'pry'; binding.pry
+    ScaffoldParser.scaffold('./spec/fixtures/xsd/invoice.xsd')
 
-    res
-    # expect(File.read('./tmp/faktura_type.rb')).to eq File.read('./spec/fixtures/exemplar/faktura_type.rb')
-  end
-
-  it 'ble' do
-    node = ScaffoldParser::Node.new
-    node.name = 'Something'
-    node2 = ScaffoldParser::Node.new
-    node2.name = 'Anything'
-    node3 = ScaffoldParser::Node.new
-    node3.name = 'Everything'
-
-    node4 = ScaffoldParser::Node.new
-    node4.name = 'LOL'
-    node3.nodes << node4
-
-    root = ScaffoldParser::Node.new
-    root.name = 'Root'
-    root.nodes << node
-    root.nodes << node2
-    root.nodes << node3
-
-    # require 'pry'; binding.pry
-    puts root
-
-    1
+    expect(File.read('./tmp/faktura_type.rb')).to eq File.read('./spec/fixtures/exemplar/faktura_type.rb')
   end
 end
