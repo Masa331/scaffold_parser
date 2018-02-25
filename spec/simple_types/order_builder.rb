ba855ed233577e974c5ce70e49145db0cf49cb3f
Premysl Donat <pdonat@seznam.cz>
2018-02-25 22:22:53 +0100

    wip

---
+++ spec/simple_types/order_builder.rb
@@ -0,0 +1,19 @@
+module Builders
+  class Order
+    include BaseBuilder
+
+    attr_accessor :name, :title, :total
+
+    def builder
+      doc = Ox::Document.new
+
+      root = Ox::Element.new('order')
+
+      root << Ox::Element.new('name') << name if name
+      root << Ox::Element.new('title') << title if title
+      root << Ox::Element.new('Total') << total if total
+
+      doc
+    end
+  end
+end
